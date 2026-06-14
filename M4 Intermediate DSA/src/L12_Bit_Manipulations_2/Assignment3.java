ad42b746155a30c9bc75624643ea79fa36cb6846
Harshal D Ganbote <harshalganbote55@gmail.com>
2026-06-14 10:54:35 +0530

    Refactor: Remove unused classes and main methods from various assignments in DSA modules
    - Deleted Main.java files from L7_Arrays_Subarrays, L8_Arrays_2D_Matrices, L9_Arrays_Sliding_Window, L12_Bit_Manipulations_2, L13_Maths_Modular_Arthimetic_Introduction, L14_Sorting, and contest modules.
    - Removed assignment classes that were not utilized: Assignment1 to Assignment7 in L8_Arrays_2D_Matrices, Assignment1 to Assignment4 in L12_Bit_Manipulations_2, Assignment1 to Assignment3 in L13_Maths_Modular_Arthimetic_Introduction, Assignment1 to Assignment4 in L14_Sorting, and Assignment1 and Assignment2 in contest_1_reattempt.
    - Deleted utility class Utils.java from util package as it was not referenced in any remaining code.

--- M4 Intermediate DSA/src/L12_Bit_Manipulations_2/Assignment3.java
+++
@@ -1,9 +0,0 @@
-package L12_Bit_Manipulations_2;
-
-public class Assignment3 {
-
-  public int solve(int A, int B) {
-    return (1 << A) | (1 << B);
-  }
-
-}
--- M4 Intermediate DSA/src/L12_Bit_Manipulations_2/Assignment4.java
+++
@@ -1,9 +0,0 @@
-package L12_Bit_Manipulations_2;
-
-public class Assignment4 {
-
-  public int solve(int A, int B) {
-    return A & ~(1 << B);
-  }
-
-}
